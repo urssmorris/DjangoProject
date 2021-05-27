ba76d1654e63abc230143dc546e1ca1cf2fad1e8
urssmorris <urssmorris@gmail.com>
2021-05-27 10:48:22 -0300

    wiki2

--- Project1/wiki/encyclopedia/urls.py
+++ Project1/wiki/encyclopedia/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("wiki", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry")
+    path("wiki/<title>", views.entry, name="entry")
 ]
--- Project1/wiki/encyclopedia/views.py
+++ Project1/wiki/encyclopedia/views.py
@@ -1,10 +1,8 @@
-from django import forms
-from django.shortcuts import render
-from markdown2 import Markdown
-
-markdowner = Markdown()
+from django.shortcuts import render, redirect
 
 from . import util
+
+from markdown2 import markdown
 
 
 def index(request):
@@ -12,18 +10,23 @@ def index(request):
         "entries": util.list_entries()
     })
 
+#Entry
 def entry(request, title):
-    entries = util.list_entries()
-    if title in entries:
-        page = util.get_entry(title)
-        page_converted = markdowner.convert(page)
+    content = util.get_entry(title.strip())
+    if content == None:
+        content = "## Page was not found"
+    content = markdown(content)
+    return render(request, "encyclopedia/entry.html", {'content': content, 'title': title})
 
-        context = {
-            'page': page_converted,
-            'title': title            
-        }
-        return render(request, "encyclopedia/entry.html", context)
+def edit(request, title):
+    content = util.get_entry(title.strip())
+    if content == None:
+        return render(request, "encyclopedia/edit.html", {'error': "404 Not Found"})
 
-    else:
-        return render(request, "encyclopedia/error.html")
-
+    if request.method == "POST":
+        content = request.POST.get("content").strip()
+        if content == "":
+            return render(request, "encyclopedia/edit.html", {"message": "Can't save with empty field.", "title": title, "content": content})
+        util.save_entry(title, content)
+        return redirect("entry", title=title)
+    return render(request, "encyclopedia/edit.html", {'content': content, 'title': title})
