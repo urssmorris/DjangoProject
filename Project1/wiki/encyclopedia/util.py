4cd7b1c38a19ae14ae126701e3d973996a956e61
urssmorris <urssmorris@gmail.com>
2021-05-28 18:12:44 -0300

    search

--- Project1/wiki/encyclopedia/util.py
+++ Project1/wiki/encyclopedia/util.py
@@ -35,3 +35,11 @@ def get_entry(title):
         return f.read().decode("utf-8")
     except FileNotFoundError:
         return None
+
+def search(query):
+    """
+    Returns a list of all names of encyclopedia entries containing the search query
+    """
+    _, filenames = default_storage.listdir("entries")
+    return list(sorted(re.sub(r"\.md$", "", filename)
+                for filename in filenames if filename.endswith(".md") and query in filename.lower()))
