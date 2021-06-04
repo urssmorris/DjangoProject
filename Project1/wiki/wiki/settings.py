b81bdc4ec2a97bbf03fb2079a4d401b1731f0502
urssmorris <urssmorris@gmail.com>
2021-06-04 20:06:54 -0300

    new

--- Project1/wiki/wiki/settings.py
+++ Project1/wiki/wiki/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# Static path
+#Static path
 STATIC_URL = '/static/encyclopedia/'
