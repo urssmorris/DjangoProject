a34725b49d66afdbcdc044cc87223815e88b4b99
urssmorris <urssmorris@gmail.com>
2021-06-23 23:25:51 -0300

    n

--- Project2/commerce/commerce/settings.py
+++ Project2/commerce/commerce/settings.py
@@ -120,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/auctions/'
+
+#django 3.2: primary keys defined to be created automatically.
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
