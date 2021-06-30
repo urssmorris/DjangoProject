8f7e19361a33c4f7fd042a007f84db5bd296b1b9
urssmorris <urssmorris@gmail.com>
2021-06-30 20:02:13 -0300

    css

--- Project2/commerce/auctions/forms.py
+++ Project2/commerce/auctions/forms.py
@@ -1,7 +1,11 @@
+from typing import AbstractSet
 from django.forms import ModelForm
 from .models import *
+
 
 class ListingForm(ModelForm):
   class Meta:
     model = Listing
     fields = ['item', 'price', 'description', 'category', 'image']
+
+   
